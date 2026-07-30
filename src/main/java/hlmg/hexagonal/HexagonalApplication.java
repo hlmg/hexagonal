e08b81947e7f287532b0c4b9a2a3423f7bf1b46d
hlmg <devhlmg@gmail.com>
2026-07-30 22:36:09 +0900

    Initial commit

---
+++ src/main/java/hlmg/hexagonal/HexagonalApplication.java
@@ -0,0 +1,13 @@
+package hlmg.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(HexagonalApplication.class, args);
+    }
+
+}
